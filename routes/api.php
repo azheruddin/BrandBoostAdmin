b0b387e4f3bba8e369986af0894652731f7dd307
Mohammad Shoeb Ansari <mshoebansari477@gmail.com>
2025-08-01 14:03:32 +0530

    api.php

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/overlay-video/{id}', [ApiController::class, 'overlayOnPostVideo']);
 
 
-
 // for post methd get-api
 Route::post('/insert-category',[ApiController::class,'insertCategory']);
 Route::get('/categories', [ApiController::class, 'getAllCategory']);
